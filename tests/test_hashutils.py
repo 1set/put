697e68d893702720bcd2953bf0ee5e51f94c1f84
Angel Analoosen <an9an63@gmail.com>
2019-08-31 21:05:10 +0800

    all tests for hashutils

--- tests/test_hashutils.py
+++ tests/test_hashutils.py
@@ -1,13 +1,23 @@
 import pytest
-from put.hashutils import md5str
-
-
-__author__ = "Angel Analoosen"
-__copyright__ = "Angel Analoosen"
-__license__ = "MIT License"
+from put.hashutils import md5str, md5sum, md5base64
 
 
 def test_md5str():
     assert md5str("") == "d41d8cd98f00b204e9800998ecf8427e"
     assert md5str("A") == "7fc56270e7a70fa81a5935b72eacbe29"
     assert md5str("123456") == "e10adc3949ba59abbe56e057f20f883e"
+    assert md5str("哦😯") == "4a552abe9132e1679d38f2ff876e63ba"
+    with pytest.raises(TypeError):
+        assert md5str(123)
+
+
+def test_md5sum():
+    assert md5sum("LICENSE") == "ec91219365c86f4951b8a3a2c1ff3ffe"
+    with pytest.raises(FileNotFoundError):
+        assert md5sum("__read_a_file_no_exists__")
+
+
+def test_md5base64():
+    assert md5base64("LICENSE") == r"7JEhk2XIb0lRuKOiwf8//g=="
+    with pytest.raises(FileNotFoundError):
+        assert md5sum("__read_a_file_no_exists__")
