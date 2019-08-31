69343762b4fa50c75a6b1992828b32d14e23ee32
Angel Analoosen <an9an63@gmail.com>
2019-09-01 00:29:51 +0800

    fix md5 sum for hashutils

--- src/put/hashutils.py
+++ src/put/hashutils.py
@@ -2,23 +2,25 @@ from base64 import b64encode
 from functools import partial
 import hashlib
 
+_file_buffer_size = 4096
 
-def _compute_md5_hash(file_name):
+
+def _compute_file_hash(file_name, calc_func):
     with open(file_name, mode="rb") as f:
-        d = hashlib.md5()
-        for buf in iter(partial(f.read, 4096), b""):
+        d = calc_func()
+        for buf in iter(partial(f.read, _file_buffer_size), b""):
             d.update(buf)
         return d
 
 
 def md5sum(file_name):
     """Calculate MD5 checksum for a file"""
-    return _compute_md5_hash(file_name).hexdigest()
+    return _compute_file_hash(file_name, hashlib.md5).hexdigest()
 
 
 def md5base64(file_name):
     """Calculate MD5 checksum in Base64 for a file"""
-    return b64encode(_compute_md5_hash(file_name).digest()).decode("utf-8")
+    return b64encode(_compute_file_hash(file_name, hashlib.md5).digest()).decode("utf-8")
 
 
 def md5str(content):
