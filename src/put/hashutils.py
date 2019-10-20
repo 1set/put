c37fccdc89c6a305413f349c2580b8754414c49e
Angel Analoosen <an9an63@gmail.com>
2019-10-20 23:45:34 +0800

    Add SHA1 & SHA256

--- src/put/hashutils.py
+++ src/put/hashutils.py
@@ -26,3 +26,33 @@ def md5sum(file_name):
 def md5base64(file_name):
     """Calculate MD5 checksum in Base64 for a file"""
     return b64encode(_compute_file_hash(file_name, hashlib.md5).digest()).decode("utf-8")
+
+
+def sha1str(content):
+    """Calculate SHA1 checksum for a string"""
+    return hashlib.sha1(str.encode(content)).hexdigest()
+
+
+def sha1sum(file_name):
+    """Calculate SHA1 checksum for a file"""
+    return _compute_file_hash(file_name, hashlib.sha1).hexdigest()
+
+
+def sha1base64(file_name):
+    """Calculate SHA1 checksum in Base64 for a file"""
+    return b64encode(_compute_file_hash(file_name, hashlib.sha1).digest()).decode("utf-8")
+
+
+def sha256str(content):
+    """Calculate SHA256 checksum for a string"""
+    return hashlib.sha256(str.encode(content)).hexdigest()
+
+
+def sha256sum(file_name):
+    """Calculate SHA256 checksum for a file"""
+    return _compute_file_hash(file_name, hashlib.sha256).hexdigest()
+
+
+def sha256base64(file_name):
+    """Calculate SHA256 checksum in Base64 for a file"""
+    return b64encode(_compute_file_hash(file_name, hashlib.sha256).digest()).decode("utf-8")
