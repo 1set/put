6a5034194f6ff164e42730060d19da780b053a7d
Angel Analoosen <an9an63@gmail.com>
2019-10-27 18:09:35 +0800

    Omit the test for Python 3.5

--- tests/test_jsonutils.py
+++ tests/test_jsonutils.py
@@ -2,7 +2,6 @@
 
 import pytest
 from put.jsonutils import (
-    SUPPORT_DATACLASS,
     dump_json,
     save_json,
     load_json,
@@ -24,34 +23,6 @@ def test_dump_json():
     sample["error"] = TemporaryDirectory
     with pytest.raises(TypeError):
         dump_json(sample, pretty_print=False)
-
-
-def test_dump_json_with_dataclass():
-    if not SUPPORT_DATACLASS:
-        pytest.skip("dataclass is only supported in Python 3.7 and later")
-
-    context = {}
-    exec(
-        """
-from put.jsonutils import dump_json
-from dataclasses import dataclass
-from datetime import datetime
-
-@dataclass
-class SampleClass:
-    Flag: bool
-    Rate: float
-    Count: int
-    Time: datetime
-
-sample = SampleClass(Flag=True,
-                    Rate=0.233333,
-                    Count=8,
-                    Time=datetime(year=2019, month=10, day=28, hour=12, minute=34, second=56))
-json = dump_json(sample, pretty_print=False)
-    """, context)
-
-    assert context['json'] == '{"Count": 8, "Flag": true, "Rate": 0.233333, "Time": "2019-10-28T12:34:56"}'
 
 
 def test_save_json():
