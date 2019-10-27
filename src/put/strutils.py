aca7dfa69e30a56c3725fe75da693d75d091028c
Angel Analoosen <an9an63@gmail.com>
2019-10-27 09:07:59 +0800

    Add docs strings for strutils

--- src/put/strutils.py
+++ src/put/strutils.py
@@ -1,26 +1,30 @@
-def str_before_first(src, trg):
-    idx = src.find(trg)
+def str_before_first(src, sub):
+    """Return a substring which starts at the begin and continues to the first occurrence of the substring sub."""
+    idx = src.find(sub)
     if idx < 0:
         return ""
     return src[:idx]
 
 
-def str_after_first(src, trg):
-    idx = src.find(trg)
+def str_after_first(src, sub):
+    """Return a substring which starts after the first occurrence of the substring sub and continues to the end."""
+    idx = src.find(sub)
     if idx < 0:
         return ""
-    return src[idx+len(trg):]
+    return src[idx+len(sub):]
 
 
-def str_before_last(src, trg):
-    idx = src.rfind(trg)
+def str_before_last(src, sub):
+    """Return a substring which starts at the begin and continues to the last occurrence of the substring sub."""
+    idx = src.rfind(sub)
     if idx < 0:
         return ""
     return src[:idx]
 
 
-def str_after_last(src, trg):
-    idx = src.rfind(trg)
+def str_after_last(src, sub):
+    """Return a substring which starts after the last occurrence of the substring sub and continues to the end."""
+    idx = src.rfind(sub)
     if idx < 0:
         return ""
-    return src[idx+len(trg):]
+    return src[idx+len(sub):]
