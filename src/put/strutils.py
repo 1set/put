1cdf67bbf282bd7afd23c73450d3683bb0543815
Angel Analoosen <53369766+an63@users.noreply.github.com>
2019-10-27 10:59:03 +0800

    Fix duplicate code, and upload test coverage reports (#23)
    * Fix duplicate code blocks
    * Upload report to DeepSource
    * Upload report to Code Climate
    * Upload report to Codacy

--- src/put/strutils.py
+++ src/put/strutils.py
@@ -1,30 +1,22 @@
 def str_before_first(src, sub):
     """Return a substring from the beginning of the string to the first occurrence of the substring sub."""
     idx = src.find(sub)
-    if idx < 0:
-        return ""
-    return src[:idx]
+    return src[:idx] if idx >= 0 else ""
 
 
 def str_after_first(src, sub):
     """Return a substring from the first occurrence of the substring sub to the end of the string."""
     idx = src.find(sub)
-    if idx < 0:
-        return ""
-    return src[idx + len(sub):]
+    return src[idx + len(sub):] if idx >= 0 else ""
 
 
 def str_before_last(src, sub):
     """Return a substring from the beginning of the string to the last occurrence of the substring sub."""
     idx = src.rfind(sub)
-    if idx < 0:
-        return ""
-    return src[:idx]
+    return src[:idx] if idx >= 0 else ""
 
 
 def str_after_last(src, sub):
     """Return a substring from the last occurrence of the substring sub to the end of the string."""
     idx = src.rfind(sub)
-    if idx < 0:
-        return ""
-    return src[idx + len(sub):]
+    return src[idx + len(sub):] if idx >= 0 else ""
